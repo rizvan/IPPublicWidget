95390b182ac07cb3b69fb908e0f9582db8855f00
genyus <anthonyfernandez16@gmail.com>
2013-10-01 23:12:25 +0200

    changes on UI update

--- src/fr/fernandezanthony/publicipwidget/Main.java
+++ src/fr/fernandezanthony/publicipwidget/Main.java
@@ -12,12 +12,14 @@ import org.json.JSONObject;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class Main extends AppWidgetProvider  {
 
@@ -25,6 +27,9 @@ public class Main extends AppWidgetProvider  {
 	private Context mcontext;
 	private DataBaseHelper myDbHelperAccount;
 	private SQLiteDatabase myDbAccount = null;
+
+	private AppWidgetManager appWidgetManagerTemp;
+	private int[] appWidgetIdsTemp;
 
 	public static String ACTION_WIDGET_CONFIGURE = "ConfigureWidget";
 	public static String WIDGET_BUTTON = "fr.fernandezanthony.publicipwidget.WIDGET_BUTTON";
@@ -39,10 +44,11 @@ public class Main extends AppWidgetProvider  {
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
 		mcontext = context;
-		//updateMethod();
+		appWidgetManagerTemp = appWidgetManager;
+		appWidgetIdsTemp = appWidgetIds;
 		ipAddress = getip(context);
-		
 		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_main);
 		remoteViews.setTextViewText(R.id.textView1, "Public IP - "+ ipAddress);
 
@@ -52,117 +58,144 @@ public class Main extends AppWidgetProvider  {
 		remoteViews.setOnClickPendingIntent(R.id.imageButton1, pendingIntent );
 
 		appWidgetManager.updateAppWidget(appWidgetIds,remoteViews);
-		super.onUpdate(context, appWidgetManager, appWidgetIds);
-	}
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		mcontext = context;
-		if (WIDGET_BUTTON.equals(intent.getAction())) {
-			updateMethod();
-			ipAddress = getip(context);
-			Log.w("tag", "Button click");
+		final int N = appWidgetIds.length;
+		for (int i=0; i<N; i++) {
+			int appWidgetId = appWidgetIds[i];
+			updateAppWidget(context, appWidgetManager, appWidgetId, getip(context)
+					);
 		}
-		super.onReceive(context, intent);
-	}
-	
-	public void updateMethod() {
-		CAsyncTask task = new CAsyncTask();
-		task.execute();
-	}
-
-	private class CAsyncTask extends AsyncTask<String, Void, String> {
+			
+		}
 
 		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
+		public void onReceive(Context context, Intent intent) {
+			mcontext = context;
+			if (WIDGET_BUTTON.equals(intent.getAction())) {
+				updateMethod();
+				ipAddress = getip(context);
+				android.text.ClipboardManager clipboard = (android.text.ClipboardManager)context.getSystemService(context.CLIPBOARD_SERVICE); 
+				clipboard.setText(getip(context)); 
+				Toast.makeText(context.getApplicationContext(), "Public IP copied to clipboard", Toast.LENGTH_SHORT).show();
+
+				AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+				ComponentName thisAppWidget = new ComponentName(context.getPackageName(), Main.class.getName());
+				int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
+
+				onUpdate(context, appWidgetManager, appWidgetIds);
+			}
+			super.onReceive(context, intent);
 		}
 
-		protected String doInBackground (String... pars) {
-			String ip = null;
-			String str;
-			try {
-				HttpClient httpclient = new DefaultHttpClient();
-				HttpGet httpget = new HttpGet("http://ip2country.sourceforge.net/ip2c.php?format=JSON");
-				// HttpGet httpget = new HttpGet("http://whatismyip.com.au/");
-				// HttpGet httpget = new HttpGet("http://www.whatismyip.org/");
-				HttpResponse response;
+		public void updateMethod() {
+			CAsyncTask task = new CAsyncTask();
+			task.execute();
+		}
 
-				response = httpclient.execute(httpget);
-				//Log.i("externalip",response.getStatusLine().toString());
+		public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+				int appWidgetId, String ip){
+			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_main);
+			remoteViews.setTextViewText(R.id.textView1, "Public IP - "+ ip);
 
-				HttpEntity entity = response.getEntity();
-				entity.getContentLength();
-				str = EntityUtils.toString(entity);
-				JSONObject json_data = new JSONObject(str);
-				ip = json_data.getString("ip");
+			appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
+
+			//Toast.makeText(context, "updateAppWidget(): " + String.valueOf(appWidgetId), Toast.LENGTH_LONG).show();
+
+		}
+
+		private class CAsyncTask extends AsyncTask<String, Void, String> {
+
+			@Override
+			protected void onPreExecute() {
+				super.onPreExecute();
+			}
+
+			protected String doInBackground (String... pars) {
+				String ip = null;
+				String str;
+				try {
+					HttpClient httpclient = new DefaultHttpClient();
+					HttpGet httpget = new HttpGet("http://ip.jsontest.com/");
+					//HttpGet httpget = new HttpGet("http://ip2country.sourceforge.net/ip2c.php?format=JSON");
+					// HttpGet httpget = new HttpGet("http://whatismyip.com.au/");
+					// HttpGet httpget = new HttpGet("http://www.whatismyip.org/");
+					HttpResponse response;
+
+					response = httpclient.execute(httpget);
+					//Log.i("externalip",response.getStatusLine().toString());
+
+					HttpEntity entity = response.getEntity();
+					entity.getContentLength();
+					str = EntityUtils.toString(entity);
+					JSONObject json_data = new JSONObject(str);
+					ip = json_data.getString("ip");
+					return ip;
+				}
+				catch (Exception e){
+					Log.e("error", "Error"+e.toString());
+				}
 				return ip;
-			}
-			catch (Exception e){
-				Log.e("error", "Error"+e.toString());
-			}
-			return ip;
-		} // doInBackground ();
+			} // doInBackground ();
 
-		protected void onPostExecute (String retour) {
-			ipAddress = retour;
-			insert(retour);
-			Log.w("tag", "IP = "+retour);
-		} // onPostExecute ();
+			protected void onPostExecute (String retour) {
+				ipAddress = retour;
+				insert(retour);
+				Log.w("tag", "IP = "+retour);
+			} // onPostExecute ();
 
-	} // CAsyncTask ();
+		} // CAsyncTask ();
 
-	public String getip(Context context) {
-		//Account retrieving
-		myDbHelperAccount = new DataBaseHelper(context);
+		public String getip(Context context) {
+			//Account retrieving
+			myDbHelperAccount = new DataBaseHelper(context);
 
-		try {
-			// A reference to the database can be obtained after initialization.
-			myDbAccount = myDbHelperAccount.getWritableDatabase();
-
-			List<IP> listip = myDbHelperAccount.getAllAccount();
-			Log.w("tag", "CURSOR size = "+listip.size());
-			return listip.get(0).getIp();
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
 			try {
-				myDbHelperAccount.close();
+				// A reference to the database can be obtained after initialization.
+				myDbAccount = myDbHelperAccount.getWritableDatabase();
+
+				List<IP> listip = myDbHelperAccount.getAllAccount();
+				Log.w("tag", "CURSOR size = "+listip.size());
+				return listip.get(listip.size()-1).getIp();
+
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			} finally {
-				myDbAccount.close();
+				try {
+					myDbHelperAccount.close();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				} finally {
+					myDbAccount.close();
+				}
 			}
+			return null;
 		}
-		return null;
-	}
 
 
-	public void insert(String tempIP) {
-		Log.w("tag", "TEMPIP = "+tempIP);
-		//Account retrieving
-		myDbHelperAccount = new DataBaseHelper(mcontext);
+		public void insert(String tempIP) {
+			Log.w("tag", "TEMPIP = "+tempIP);
+			//Account retrieving
+			myDbHelperAccount = new DataBaseHelper(mcontext);
 
-		try {
-			// A reference to the database can be obtained after initialization.
-			myDbAccount = myDbHelperAccount.getWritableDatabase();
-			myDbHelperAccount.deleteAll();
-			
-			IP ip = new IP();
-			ip.setIp(tempIP);
-
-			myDbHelperAccount.addAccount(ip);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
 			try {
-				myDbHelperAccount.close();
+				// A reference to the database can be obtained after initialization.
+				myDbAccount = myDbHelperAccount.getWritableDatabase();
+				myDbHelperAccount.deleteAll();
+
+				IP ip = new IP();
+				ip.setIp(tempIP);
+
+				myDbHelperAccount.addAccount(ip);
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			} finally {
-				myDbAccount.close();
+				try {
+					myDbHelperAccount.close();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				} finally {
+					myDbAccount.close();
+				}
 			}
 		}
 	}
-}
